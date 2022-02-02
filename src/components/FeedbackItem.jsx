2a109e631c81ed89e60da01612d742a0990f18d0
tapia1013 <manbearpig1013@gmail.com>
2022-02-02 15:00:45 -0800

    added propTypes

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Card from './shared/Card';
 
 function FeedbackItem({ item }) {
@@ -10,6 +11,11 @@ function FeedbackItem({ item }) {
       </div>
     </Card>
   )
+}
+
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList({ feedback }) {
@@ -13,6 +14,16 @@ function FeedbackList({ feedback }) {
         <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
   )
 }
 
