b4f9188a9b7444e02471c82fe40d0b960f9c2394
tapia1013 <manbearpig1013@gmail.com>
2022-02-02 17:06:36 -0800

    feedbackform validation inputs

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    // validation
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters long');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false)
+    }
     setText(e.target.value);
   }
 
@@ -24,8 +37,13 @@ function FeedbackForm() {
           <Button t
             ype="submit"
             version="secondary"
-          >Send</Button>
+            isDisabled={btnDisabled}
+          >
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
